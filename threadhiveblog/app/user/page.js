7525979bf2653b5232f46985b0ec4566527baf8e
dornor <suchaya_a@cmu.ac.th>
2025-01-24 14:41:27 +0700

    user update

--- threadhiveblog/app/user/page.js
+++ threadhiveblog/app/user/page.js
@@ -1,1 +1,20 @@
-//fetch User
+import NavBar from "../components/Navbar";
+import SideBar from "../components/Sidebar";
+
+export default function User() {
+    return(
+        <div>
+            {/* <NavBar/>
+            <SideBar/> */}
+            <div className="p-16">
+                <div className="bg-orange-500 h-full">
+                    test
+                </div>
+            </div>
+
+
+        </div>
+
+    )
+
+}
