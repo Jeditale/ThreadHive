756e273b070eef69b3364ff3b43a34dbf51ad239
Jeditale <69350223+Jeditale@users.noreply.github.com>
2025-01-28 16:18:44 +0700

    made swagger remember token and when refresh page keep token

--- thread-hive-backend/src/main.ts
+++ thread-hive-backend/src/main.ts
@@ -15,14 +15,21 @@ async function bootstrap() {
       {
         type: 'http',
         scheme: 'bearer',
+        name:'jwt',
         bearerFormat: 'JWT',  // Optional, just for clarity
+        in:'header',
       },
       'jwt',  // This match the name of strategy
     )
+    .addSecurityRequirements('jwt')
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api', app, document ,{
+    swaggerOptions: {
+      persistAuthorization: true, // this
+  },
+  })
 
   await app.listen(3000);
 }
