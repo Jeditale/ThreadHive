5c03fa67699bc4e1efcbe5fc0e37545bd26a9746
Jeditale <69350223+Jeditale@users.noreply.github.com>
2025-01-29 14:23:04 +0700

    added experimental (cypress Studio) into cypress.config

--- threadhiveblog/cypress.config.js
+++ threadhiveblog/cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   projectId: 'pnmud5',
   e2e: {
+    experimentalStudio: true,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
