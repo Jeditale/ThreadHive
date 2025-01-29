586ead1bc0e5da92035df05a753cb1723542f25a
dornor <suchaya_a@cmu.ac.th>
2025-01-30 01:05:19 +0700

    On my way edit profile

--- threadhiveblog/app/user/editProfile/page.js
+++ threadhiveblog/app/user/editProfile/page.js
@@ -3,9 +3,34 @@
 import NavBar from "@/app/components/Navbar";
 import SideBar from "@/app/components/Sidebar";
 import Link from 'next/link';
+import { useState,useEffect } from "react";
 import Swal from "sweetalert2";
 
 export default function EditForm() {
+
+    const [user, setUser] = useState([])
+
+    const base64Pic = "data:image/png;base64,"
+
+    useEffect(() => {
+        async function getUser() {
+            const userId = sessionStorage.getItem("userId")
+            const response = await fetch(`https://threadhive.onrender.com/users/${userId}`,{
+                headers : {
+                    'Authorization': `Bearer ${sessionStorage.getItem('userToken')}`
+                }
+            })
+            if (!response.ok) {
+                throw new Error("cannot fetch");
+            }
+            const userData = await response.json();
+            setUser(userData)
+        }
+        getUser()
+        
+    }, []);
+
+
 
     return (
         <div className="bg-[#FAF3B8] dark:bg-[#3A2E2A] min-h-screen">
@@ -15,26 +40,26 @@ export default function EditForm() {
                 <div className="col-start-3 col-span-3">
                     <div className="bg-[#FFF8DC] dark:bg-[#5b4e4a] rounded-lg shadow-lg w-full max-w-4xl mb-6 p-24 pt-10 pb-10">
                         <div className="flex flex-col items-center">
-                            <img src="/assets/profile.png" className="w-36 h-36" />
+                            <img src={base64Pic+user.profilePicture} className="w-36 h-36 rounded-full" />
                             <button className="bg-[#FAF3B8] dark:bg-[#FEF7D8] hover:bg-[#EAC67A] dark:hover:bg-[#ddd09a] rounded-2xl shadow-lg p-2 mt-5 mb-7">แก้ไขรูปโปรไฟล์</button>
                         </div>
 
                         <label className="dark:text-white">ชื่อผู้ใช้</label>
                         <div>
-                            <input type="text" name="name" placeholder="ชื่อผู้ใช้"
+                            <input type="text" name="name" placeholder={user.usrname}
                             className="bg-white dark:bg-[#FEF7D8] shadow-lg rounded-2xl w-full p-2 mt-1 mb-3"></input>
                         </div>
 
                         <div className="flex space-x-4 items-center justify-center">
                             <div className="flex flex-col flex-grow">
                                 <label className="dark:text-white">ชื่อ</label>
-                                <input type="text" name="fname" placeholder="ชื่อ"
+                                <input type="text" name="fname" placeholder={user.fname}
                                 className="bg-white dark:bg-[#FEF7D8] shadow-lg rounded-2xl w-full p-2 mt-1 mb-3"></input>
                             </div>
                             
                             <div className="flex flex-col flex-grow">
                                 <label className="dark:text-white">นามสกุล</label>
-                                <input type="text" name="lname" placeholder="นามสุล"
+                                <input type="text" name="lname" placeholder={user.lname}
                                 className="bg-white dark:bg-[#FEF7D8] shadow-lg rounded-2xl w-full p-2 mt-1 mb-3"></input>
                             </div>
                         </div>
@@ -43,7 +68,7 @@ export default function EditForm() {
                         <div>
                             <select name="gender" placeholder="เพศ"
                             className="bg-white dark:bg-[#FEF7D8] shadow-lg rounded-2xl w-full p-2 mt-1 mb-3">
-                                <option value="" disabled selected>เพศ</option>
+                                <option value="" disabled selected>{user.sex}</option>
                                 <option value="male">ชาย</option>
                                 <option value="female">หญิง</option>
                                 <option value="other">อื่นๆ</option>
--- threadhiveblog/app/user/page.js
+++ threadhiveblog/app/user/page.js
@@ -17,7 +17,7 @@ function logout() {
 }
 
 export default function User() {
-    const [post, setPost] = useState([]); 
+    const [posts, setPost] = useState([]); 
     const [user, setUser] = useState([])
     // const [loading, setLoading] = useState(true); 
     // const [error, setError] = useState(null); 
@@ -25,19 +25,6 @@ export default function User() {
     const base64Pic = "data:image/png;base64,"
 
     useEffect(() => {
-        async function getPost() {
-            const userId = sessionStorage.getItem("userId")
-            const response = await fetch(`https://threadhive.onrender.com/posts/user/${userId}`, {
-                headers : {
-                    'Authorization': `Bearer ${sessionStorage.getItem('userToken')}`
-                }
-            });
-            if (!response.ok) {
-                throw new Error("cannot fetch");
-            }
-            return response.json();
-        }
-        
         async function getUser() {
             const userId = sessionStorage.getItem("userId")
             const response = await fetch(`https://threadhive.onrender.com/users/${userId}`,{
@@ -48,8 +35,26 @@ export default function User() {
             if (!response.ok) {
                 throw new Error("cannot fetch");
             }
-            return response.json();
+            const userData = await response.json();
+            setUser(userData)
         }
+
+        async function getPostFromUser() {
+            const userId = sessionStorage.getItem("userId")
+            const response = await fetch(`https://threadhive.onrender.com/posts/user/${userId}`,{
+                headers : {
+                    'Authorization': `Bearer ${sessionStorage.getItem('userToken')}`
+                }
+            })
+            if (!response.ok) {
+                throw new Error("cannot fetch");
+            }
+            const postData = await response.json();
+            setPost(postData)
+        }
+
+        getUser()
+        getPostFromUser()
         
     }, []);
 
@@ -131,102 +136,107 @@ export default function User() {
                 <div className="col-start-3 col-span-3">
 
                 <p className="text-lg pb-4 dark:text-white">โพสต์ทั้งหมด</p>
+                
+                    <div>
+                        {posts.map((post, index) => (
+                            <div key={index} className="bg-[#FFF8DC] dark:bg-[#5b4e4a] p-6 rounded-lg shadow-lg w-full max-w-4xl mb-6">
 
-                    <div className="bg-[#FFF8DC] dark:bg-[#5b4e4a] p-6 rounded-lg shadow-lg w-full max-w-4xl mb-6">
+                            {/*  ส่วนโปรไฟล์, ชื่อ และวันที่โพสต์ */}
+                            <div className="relative flex items-center space-x-3 mb-3">
+                                {/* รูปโปรไฟล์ */}
+                                <img src={base64Pic+user.profilePicture} alt="Profile" className="w-12 h-12 rounded-full" />
 
-                        {/*  ส่วนโปรไฟล์, ชื่อ และวันที่โพสต์ */}
-                        <div className="relative flex items-center space-x-3 mb-3">
-                            {/* รูปโปรไฟล์ */}
-                            <img src={user.profilePicture} alt="Profile" className="w-12 h-12 rounded-full" />
+                                {/* ชื่อและวันที่ */}
+                                <div className="flex-1 min-w-0">
+                                    <p className="font-semibold dark:text-white truncate">{user.usrname}</p>
+                                    <p className="text-gray-500 text-sm dark:text-white">
+                                        {new Date(post.createdAt).toLocaleDateString("th-TH", {
+                                            day: "2-digit",
+                                            month: "long",
+                                            year: "numeric",
+                                        })}
+                                    </p>
+                                </div>
 
-                            {/* ชื่อและวันที่ */}
-                            <div className="flex-1 min-w-0">
-                                <p className="font-semibold dark:text-white truncate">{user.usrname}</p>
-                                <p className="text-gray-500 text-sm dark:text-white">
-                                    {new Date(post.createdAt).toLocaleDateString("th-TH", {
-                                        day: "2-digit",
-                                        month: "long",
-                                        year: "numeric",
-                                    })}
-                                </p>
+                                {/* ปุ่มแก้ไขและลบ */}
+                                <div className="absolute top-0 right-0 flex space-x-2">
+                                    <Link href="/user/editPost" className="bg-[#3A3000] text-white hover:bg-[#2A1C08] shadow-lg px-4 py-1 rounded-2xl">
+                                        แก้ไข
+                                    </Link>
+                                    <button
+                                        className="bg-[#960000] text-white hover:bg-[#690000] shadow-lg px-6 py-1 rounded-2xl"
+                                        onClick={() => {
+                                            Swal.fire({
+                                                title: "คุณแน่ใจหรือไม่?",
+                                                text: "หากลบแล้วจะไม่สามารถกู้คืนโพสต์นี้ได้!",
+                                                icon: "warning",
+                                                showCancelButton: true,
+                                                confirmButtonColor: "#d33",
+                                                cancelButtonColor: "#3085d6",
+                                                confirmButtonText: "ใช่",
+                                                cancelButtonText: "ยกเลิก",
+                                                customClass: {
+                                                    popup: "rounded-xl shadow-xl",
+                                                    confirmButton: "bg-red-600 hover:bg-red-700 text-white px-4 py-2 rounded-lg",
+                                                    cancelButton: "bg-gray-300 hover:bg-gray-400 text-black px-4 py-2 rounded-lg",
+                                                },
+                                            }).then((result) => {
+                                                if (result.isConfirmed) {
+                                                    Swal.fire({
+                                                        title: "ลบโพสต์สำเร็จ!",
+                                                        text: "โพสต์ของคุณถูกลบเรียบร้อยแล้ว",
+                                                        icon: "success",
+                                                        confirmButtonColor: "#3085d6",
+                                                        confirmButtonText: "ตกลง",
+                                                    });
+                                                    // เพิ่มโค้ดสำหรับลบโพสต์ที่นี่
+                                                }
+                                            });
+                                        }}
+                                    >
+                                        ลบ
+                                    </button>
+                                </div>
                             </div>
+                            {/* ส่วนข้อความ */}
+                            <Link href={`/home/user/${sessionStorage.getItem("userId")}`}>
+                                <div className="mb-5">
+                                    <h3 className="font-bold text-lg dark:text-white">{post.title}</h3>
+                                    <p className="text-gray-700 dark:text-white">{post.details}</p>
+                                </div>
+                            </Link>
 
-                            {/* ปุ่มแก้ไขและลบ */}
-                            <div className="absolute top-0 right-0 flex space-x-2">
-                                <Link href="/user/editPost" className="bg-[#3A3000] text-white hover:bg-[#2A1C08] shadow-lg px-4 py-1 rounded-2xl">
-                                    แก้ไข
-                                </Link>
-                                <button
-                                    className="bg-[#960000] text-white hover:bg-[#690000] shadow-lg px-6 py-1 rounded-2xl"
-                                    onClick={() => {
-                                        Swal.fire({
-                                            title: "คุณแน่ใจหรือไม่?",
-                                            text: "หากลบแล้วจะไม่สามารถกู้คืนโพสต์นี้ได้!",
-                                            icon: "warning",
-                                            showCancelButton: true,
-                                            confirmButtonColor: "#d33",
-                                            cancelButtonColor: "#3085d6",
-                                            confirmButtonText: "ใช่",
-                                            cancelButtonText: "ยกเลิก",
-                                            customClass: {
-                                                popup: "rounded-xl shadow-xl",
-                                                confirmButton: "bg-red-600 hover:bg-red-700 text-white px-4 py-2 rounded-lg",
-                                                cancelButton: "bg-gray-300 hover:bg-gray-400 text-black px-4 py-2 rounded-lg",
-                                            },
-                                        }).then((result) => {
-                                            if (result.isConfirmed) {
-                                                Swal.fire({
-                                                    title: "ลบโพสต์สำเร็จ!",
-                                                    text: "โพสต์ของคุณถูกลบเรียบร้อยแล้ว",
-                                                    icon: "success",
-                                                    confirmButtonColor: "#3085d6",
-                                                    confirmButtonText: "ตกลง",
-                                                });
-                                                // เพิ่มโค้ดสำหรับลบโพสต์ที่นี่
-                                            }
-                                        });
-                                    }}
-                                >
-                                    ลบ
-                                </button>
-                            </div>
-                        </div>
-                        {/* ส่วนข้อความ */}
-                        <Link href={`/home/user/${sessionStorage.getItem("userId")}`}>
-                            <div className="mb-5">
-                                <h3 className="font-bold text-lg dark:text-white">{post.title}</h3>
-                                <p className="text-gray-700 dark:text-white">{post.details}</p>
-                            </div>
-                        </Link>
+                            {/* ส่วนรูปภาพ */}
+                            {post.image && (
+                                <div className="mt-2">
+                                    <img src={base64Pic+(post.image)} alt="Post Image" className="w-full rounded-lg" />
+                                </div>
+                            )}
 
-                        {/* ส่วนรูปภาพ */}
-                        {post.image && (
-                            <div className="mt-2">
-                                <img src={post.image} alt="Post Image" className="w-full rounded-lg" />
+                            {/* ปุ่ม Like, Comment, Share */}
+                            <div className="mt-4 flex items-center justify-between">
+                                <div className="flex items-center space-x-5 mt-4 text-gray-600 dark:text-black">
+                                    <button className="flex items-center space-x-1 bg-white dark:bg-[#cdc5a4] hover:bg-[#EAC67A] dark:hover:bg-[#afa87f] p-2 rounded-2xl shadow-lg">
+                                        <span className="text-xl">
+                                            <img src="/assets/like.png" alt="Home" className="w-6 h-6 mr-2" />
+                                        </span>{post.likes ?? 0}ถูกใจ
+                                    </button>
+                                    <button className="flex items-center space-x-1 bg-white dark:bg-[#cdc5a4] hover:bg-[#EAC67A] dark:hover:bg-[#afa87f] p-2 rounded-2xl shadow-lg">
+                                        <span className="text-xl">
+                                            <img src="/assets/comment.png" alt="Home" className="w-6 h-6 mr-2" />
+                                        </span>{post.comments ?? 0}ความคิดเห็น
+                                    </button>
+                                    <button className="flex items-center space-x-1 bg-white dark:bg-[#cdc5a4] hover:bg-[#EAC67A] dark:hover:bg-[#afa87f] p-2 rounded-2xl shadow-lg">
+                                        <span className="text-xl">
+                                            <img src="/assets/share.png" alt="Home" className="w-6 h-6 mr-2" />
+                                        </span>{post.shares ?? 0}แชร์
+                                    </button>
+                                </div>
                             </div>
-                        )}
-
-                        {/* ปุ่ม Like, Comment, Share */}
-                        <div className="mt-4 flex items-center justify-between">
-                            <div className="flex items-center space-x-5 mt-4 text-gray-600 dark:text-black">
-                                <button className="flex items-center space-x-1 bg-white dark:bg-[#cdc5a4] hover:bg-[#EAC67A] dark:hover:bg-[#afa87f] p-2 rounded-2xl shadow-lg">
-                                    <span className="text-xl">
-                                        <img src="/assets/like.png" alt="Home" className="w-6 h-6 mr-2" />
-                                    </span>{post.likes ?? 0}ถูกใจ
-                                </button>
-                                <button className="flex items-center space-x-1 bg-white dark:bg-[#cdc5a4] hover:bg-[#EAC67A] dark:hover:bg-[#afa87f] p-2 rounded-2xl shadow-lg">
-                                    <span className="text-xl">
-                                        <img src="/assets/comment.png" alt="Home" className="w-6 h-6 mr-2" />
-                                    </span>{post.comments ?? 0}ความคิดเห็น
-                                </button>
-                                <button className="flex items-center space-x-1 bg-white dark:bg-[#cdc5a4] hover:bg-[#EAC67A] dark:hover:bg-[#afa87f] p-2 rounded-2xl shadow-lg">
-                                    <span className="text-xl">
-                                        <img src="/assets/share.png" alt="Home" className="w-6 h-6 mr-2" />
-                                    </span>{post.shares ?? 0}แชร์
-                                </button>
                             </div>
-                        </div>
+                        ))}
                     </div>
+                    
                 </div>
             </div>
         </div>
